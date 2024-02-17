f5935eb37d8ede49de2db1f2da28a2f04d508ac1
ashuashutosh2211 <111591264+ashuashutosh2211@users.noreply.github.com>
2024-02-18 02:33:51 +0530

    Update app3.py

--- app3.py
+++ app3.py
@@ -33,57 +33,60 @@ def main():
 
     stocks = selected_tickers
     if st.button("Submit"):
-        
-        col1, col2 = st.columns(2)
-        with col1:
-            st.markdown("<h5>Selected Companies</h5>", unsafe_allow_html=True)
-            for company in selected_companies:
-                st.write(company)
-
-        with col2:
-            st.markdown("<h5>Tickers</h5>", unsafe_allow_html=True)
-            for ticker in selected_tickers:
-                st.write(ticker)
-        
-        start_date = pd.Timestamp.now() - pd.DateOffset(months=3)
-        end_date = pd.Timestamp.now()
-
-        prices = prices = yf.download(stocks, start=start_date, end=end_date)['Adj Close']
-        prices.fillna(prices.mean(), inplace=True)
-        # st.title("Markowitz Optimization Results ") 
-        portfolio_optimizer = None 
-        if( allow_short_selling == "Yes"):
-            portfolio_optimizer = shortselling.PortfolioOptimizer(prices)
-        else : 
-            portfolio_optimizer = no_short_selling.PortfolioOptimizer(prices)
-
-        
-        portfolio_optimizer.markowitz_optimization()
-        weights , expected_return = portfolio_optimizer.markowitz_optimization_max_return( risk_tolerance/100 ) 
-        exp = round(expected_return , 4) * 100 
-        st.markdown(f"<h5>Expected Return is {exp} %</h5>" , unsafe_allow_html=True)
-        # st.write(weights)
-        col1, col2 = st.columns(2)
-        with col1:
-            st.markdown("<h5>Selected Companies</h5>", unsafe_allow_html=True)
-            for company in selected_companies:
-                st.write(company)
-
-        with col2:
-            st.markdown("<h5>Weights </h5>", unsafe_allow_html=True)
-            for w in weights:
-                w = round(w , 4 ) * 100 
-                st.write(f"{w}%")
         try:
-            fig = portfolio_optimizer.plot_efficient_frontier_for_given_risk_tolerance(target_return/100) 
-            st.plotly_chart(fig)
-        except:
-            returns = portfolio_optimizer.returns
-            min_risk = returns.std().min() * 100 
-            max_risk = returns.std().max() * 100 
-            st.write("Selected Risk Tolerance Level can't be achieved ") 
-            st.write(f"Minimum Risk : {min_risk}") 
-            st.write(f"Maximum Risk : {max_risk}")
+            col1, col2 = st.columns(2)
+            with col1:
+                st.markdown("<h5>Selected Companies</h5>", unsafe_allow_html=True)
+                for company in selected_companies:
+                    st.write(company)
+    
+            with col2:
+                st.markdown("<h5>Tickers</h5>", unsafe_allow_html=True)
+                for ticker in selected_tickers:
+                    st.write(ticker)
+            
+            start_date = pd.Timestamp.now() - pd.DateOffset(months=3)
+            end_date = pd.Timestamp.now()
+    
+            prices = prices = yf.download(stocks, start=start_date, end=end_date)['Adj Close']
+            prices.fillna(prices.mean(), inplace=True)
+            # st.title("Markowitz Optimization Results ") 
+            portfolio_optimizer = None 
+            if( allow_short_selling == "Yes"):
+                portfolio_optimizer = shortselling.PortfolioOptimizer(prices)
+            else : 
+                portfolio_optimizer = no_short_selling.PortfolioOptimizer(prices)
+    
+            
+            portfolio_optimizer.markowitz_optimization()
+            weights , expected_return = portfolio_optimizer.markowitz_optimization_max_return( risk_tolerance/100 ) 
+            exp = round(expected_return , 4) * 100 
+            st.markdown(f"<h5>Expected Return is {exp} %</h5>" , unsafe_allow_html=True)
+            # st.write(weights)
+            col1, col2 = st.columns(2)
+            with col1:
+                st.markdown("<h5>Selected Companies</h5>", unsafe_allow_html=True)
+                for company in selected_companies:
+                    st.write(company)
+    
+            with col2:
+                st.markdown("<h5>Weights </h5>", unsafe_allow_html=True)
+                for w in weights:
+                    w = round(w , 4 ) * 100 
+                    st.write(f"{w}%")
+            try:
+                fig = portfolio_optimizer.plot_efficient_frontier_for_given_risk_tolerance(target_return/100) 
+                st.plotly_chart(fig)
+            except:
+                returns = portfolio_optimizer.returns
+                min_risk = returns.std().min() * 100 
+                max_risk = returns.std().max() * 100 
+                st.write("Selected Risk Tolerance Level can't be achieved ") 
+                st.write(f"Minimum Risk : {min_risk}") 
+                st.write(f"Maximum Risk : {max_risk}")
+         except:
+            st.error("An error occurred while processing the data. Please ensure that the data is available and try again.")
 
+        
 if __name__ == "__main__":
     main()
